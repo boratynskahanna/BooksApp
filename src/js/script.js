823d0921b0b839138134a6680754f4fd024d8d75
Hanna Boratynska <boratynskahanna@gmail.com>
2021-04-10 22:30:42 +0400

    Begin BooksApp

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,37 @@
 {
-    'use strict';
+  'use strict';
   
   
-    const select = {
-      templateOf: {
-        book: '#template-book',
-      },
-      containerOf: {
-        bookList: '.books-list',
-      },
-    };
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    containerOf: {
+      bookList: '.books-list',
+    },
+  };
   
-    const templates = {
-      book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-    };
+  const templates = {
+    book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+  };
   
-    const render = function() {
+  const render = function() {
     
   
-      for(let book in dataSource.books) {
+    for(let book in dataSource.books) {
       // generate HTML of the book
-        const generatedHTML = templates.book(book);    
-        // generate DOM
-        const domElement = utils.createDOMFromHTML(generatedHTML); 
-        // find book list container
-        const bookContainer = document.querySelector(select.containerOf.bookList);
+      const generatedHTML = templates.book(book);    
+      // generate DOM
+      const domElement = utils.createDOMFromHTML(generatedHTML); 
+      // find book list container
+      const bookContainer = document.querySelector(select.containerOf.bookList);
   
-        // add DOM to book list
-        bookContainer.appendChild(domElement); 
-      }
-    };
+      // add DOM to book list
+      bookContainer.appendChild(domElement); 
+    }
+  };
   
-    render();
+  render();
   
-  }
+}
+
