d244d0cbb5c72eca93e05200f6855df172faeaf0
Hanna Boratynska <boratynskahanna@gmail.com>
2021-04-10 22:22:37 +0400

    Begin

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+{
+    'use strict';
+  
+  
+    const select = {
+      templateOf: {
+        book: '#template-book',
+      },
+      containerOf: {
+        bookList: '.books-list',
+      },
+    };
+  
+    const templates = {
+      book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+    };
+  
+    const render = function() {
+    
+  
+      for(let book in dataSource.books) {
+      // generate HTML of the book
+        const generatedHTML = templates.book(book);    
+        // generate DOM
+        const domElement = utils.createDOMFromHTML(generatedHTML); 
+        // find book list container
+        const bookContainer = document.querySelector(select.containerOf.bookList);
+  
+        // add DOM to book list
+        bookContainer.appendChild(domElement); 
+      }
+    };
+  
+    render();
+  
+  }
