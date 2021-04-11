5877fb8d82c0036122a24f93cd8a67d7314d4cc2
Hanna Boratynska <boratynskahanna@gmail.com>
2021-04-11 11:52:32 +0400

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 {
+
   'use strict';
-  
   
   const select = {
     templateOf: {
@@ -8,30 +8,60 @@
     },
     containerOf: {
       bookList: '.books-list',
+      bookImage: '.book__image',
     },
   };
   
   const templates = {
     book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
   };
+
+  const classNames = {
+    bookClass: {
+      bookFavorite: 'favorite',
+    }
+  };
+  
   
   const render = function() {
+    const thisBook = this;
+    thisBook.data = dataSource.books;
     
-  
-    for(let book in dataSource.books) {
+    for(const book of thisBook.data) {
       // generate HTML of the book
       const generatedHTML = templates.book(book);    
       // generate DOM
-      const domElement = utils.createDOMFromHTML(generatedHTML); 
+      thisBook.element = utils.createDOMFromHTML(generatedHTML); 
       // find book list container
       const bookContainer = document.querySelector(select.containerOf.bookList);
   
       // add DOM to book list
-      bookContainer.appendChild(domElement); 
+      bookContainer.appendChild(thisBook.element); 
     }
   };
+
+  const favoriteBooks = [];
+  const initActions = function() {
+    
+    const bookImages = document.querySelectorAll(select.containerOf.bookImage);
+
+    for (let bookImage of bookImages) {
+      bookImage.addEventListener('dblclick', function(event) {
+        event.preventDefault();
+        bookImage.classList.add(classNames.bookClass.bookFavorite);
+        let id = bookImage.getAttribute('data-id');
+        console.log(id);
+        favoriteBooks.push(id);
+
+      });
+    }
+    console.log(favoriteBooks);
+
+   
+  };
   
+
   render();
-  
+  initActions();
 }
 
